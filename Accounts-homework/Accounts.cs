05d8108f11ce65d52d83637cf534249ce6b7f50e
LIU Lin <joel.liu@qq.com>
2019-06-16 20:18:57 +0800

    Add homework of Accounts project

---
+++ Accounts-homework/Accounts.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AccountItem;
+
+namespace AccountsApp
+{
+    public class Accounts
+    {
+        private List<Item> accounts = new List<Item>();
+
+        public void AddItem(Item item)
+        {
+            accounts.Add(item);
+        }
+
+        public Money TotalRevenue(DateTime time)
+        {
+            Money revenue = new Money(0);
+
+            foreach (Item item in accounts)
+            {
+                if (item.OccuredTime.Month == time.Month)
+                {
+                    if (item.category == Category.Spending)
+                    {
+                        revenue -= item.Amount;
+                    }
+                    else
+                    {
+                        revenue += item.Amount;
+                    }
+                }
+            }
+
+            return revenue;
+        }
+
+        public void DisplayTotalRevenue(DateTime time)
+        {
+            Money TotalRevenue = this.TotalRevenue(time);
+            Console.WriteLine("Revenue(" + time.ToString("MMMM") + "): " + TotalRevenue);
+        }
+
+        public Money TotalExpenditure(DateTime time)
+        {
+            Money expend = new Money(0);
+
+            foreach (Item item in accounts)
+            {
+                if (item.OccuredTime.Month == time.Month)
+                {
+                    if (item.category == Category.Spending)
+                    {
+                        expend += item.Amount;
+                    }
+                }
+            }
+
+            return expend;
+        }
+
+        public void DisplayTotalExpenditure(DateTime time)
+        {
+            Money TotalExpenditure = this.TotalExpenditure(time);
+            Console.WriteLine("Expend(" + time.ToString("MMMM") + "): " + TotalExpenditure);
+        }
+
+        public Money TotalIncome(DateTime time)
+        {
+            Money income = new Money(0);
+
+            foreach (Item item in accounts)
+            {
+                if (item.OccuredTime.Month == time.Month)
+                {
+                    if (item.category == Category.Income)
+                    {
+                        income += item.Amount;
+                    }
+                }
+            }
+
+            return income;
+        }
+
+        public void DisplayTotalIncome(DateTime time)
+        {
+            Money TotalIncome = this.TotalIncome(time);
+            Console.WriteLine("Income(" + time.ToString("MMMM") + "): " + TotalIncome);
+        }
+
+        public IEnumerable<Item> Display(DateTime time)
+        {
+            foreach (Item item in accounts)
+            {
+                if (item.OccuredTime.Month == time.Month)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public void DisplayAllItems()
+        {
+            foreach (Item item in accounts)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
---
+++ Accounts-homework/Item.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace AccountItem
+{
+    public enum Category : int
+    {
+        Spending = 0,
+        Income = 1
+    }
+
+    public class Item
+    {
+        protected string Name
+        {
+            get; set;
+        }
+        protected string Content
+        {
+            get; set;
+        }
+        protected string Note
+        {
+            get; set;
+        }
+
+
+        public Category category;
+        private string type
+        {
+            get => category == Category.Spending
+                ? "-"
+                : "+";
+        }
+
+
+        public Money Amount;
+
+        public DateTime OccuredTime;
+        //public string Month
+        //{
+        //    get => OccuredTime.ToString("MMMM");
+        //}
+
+        public Item(string name, Category category, double amount) : this(name, category,"None", "None", amount, Currency.RMB, DateTime.Now) { }
+        public Item(string name, Category category, double amount, DateTime occuredTime) : this(name, category, "None", "None", amount, Currency.RMB, occuredTime) { }
+        public Item(string name, Category category, string content, double amount) : this(name, category, content, "None", amount, Currency.RMB, DateTime.Now) { }
+        public Item(string name, Category category, double amount, Currency currency): this(name, category, "None", "None", amount, currency, DateTime.Now) { }
+        public Item(string name, Category category, string content, double amount, Currency currency) : this(name, category, content, "None", amount, currency, DateTime.Now) { }
+        public Item(string name, Category category, string content, string note, double amount, Currency currency, DateTime occuredTime)
+        {
+            if (name.Equals(""))
+            {
+                ArgumentException myEx = new ArgumentException("Input a valid name!");
+                throw myEx;
+            }
+
+            this.Name = name;
+            this.category = category;
+            this.Content = content;
+            this.Note = note;
+            this.Amount = new Money(amount, currency);
+            this.OccuredTime = occuredTime;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Name} ({OccuredTime.ToString("MMMM dd, yyyy")}): {type}{Amount.ToString()}. | Content: {Content} | Note: {Note}";
+        }
+
+    }
+}
