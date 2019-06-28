a2cf463919d720d70eda2f19ca49d2687e0b65a9
LIU Lin <joel.liu@qq.com>
2019-06-28 21:39:11 +0800

    Add OvenApp (learn delegate and events); Add solution file for previous apps;

---
+++ OvenApp/Oven.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace OvenApp
+{
+
+    public delegate void DoSomething();
+
+    public class Oven
+    {
+
+        public event DoSomething OverHeatEventRaiser;
+
+        private int Temperature;
+        public bool IsOn = false;
+
+        public int ThresholdTemperature
+        {
+            get; set;
+        }
+
+        public Oven() : this(37, 100) { }
+        public Oven(int thresholdTemperature): this(37, thresholdTemperature) { }
+        public Oven(int temperature, int thresholdTemperature)
+        {
+            Temperature = temperature;
+            ThresholdTemperature = thresholdTemperature;
+        }
+
+        public void Run()
+        {
+            SetOn();
+
+            while (IsOn)
+            {
+                Heat();
+                if (Temperature >= ThresholdTemperature)
+                {
+                    OverHeatEventRaiser();
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void SetOff()
+        {
+            IsOn = false;
+            Console.WriteLine("Oven is turned off.");
+        }
+
+        public void SetOn()
+        {
+            IsOn = true;
+            Console.WriteLine("Oven is turned on. Begin heat.");
+        }
+
+        private void Heat()
+        {
+            Temperature += 1;
+            Console.WriteLine("The temperature is: {0}°C", Temperature);
+        }
+
+    }
+}
---
+++ OvenApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OvenApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Oven oven = new Oven(45);
+            oven.OverHeatEventRaiser += new DoSomething(() => { Console.WriteLine("Alarm!"); });
+            oven.OverHeatEventRaiser += new DoSomething(() => { oven.SetOff(); });
+
+            oven.Run();
+
+        }
+    }
+}
